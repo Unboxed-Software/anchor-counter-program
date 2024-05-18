95e3dae9c9df661f3960d07b792f5f65c01f3733
ASCorreia <36857753+ASCorreia@users.noreply.github.com>
2024-05-18 14:42:28 +0100

    Updated to anchor 0.30 and solana 1.18
    - Updated to anchor 0.30
    
    - Updated to solana 1.18
    
    - wallet path is now generic
    
    - used InitSpace derive macro to calculate struct size
    
    - added anchor discriminator constant
    
    - added minor comments

--- programs/anchor-counter/src/lib.rs
+++ programs/anchor-counter/src/lib.rs
@@ -2,20 +2,29 @@ use anchor_lang::prelude::*;
 
 declare_id!("9sMy4hnC9MML6mioESFZmzpntt3focqwUq1ymPgbMf64");
 
+// Size of the anchor discrimitaror, needed for the space calculation.
+const ANCHOR_DISCRIMINATOR: usize = 8;
+
 #[program]
 pub mod anchor_counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Initialize the counter account with a count of 0.
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
+
+        // Log the current count.
         msg!("Counter account created. Current count: {}", counter.count);
         Ok(())
     }
 
     pub fn increment(ctx: Context<Update>) -> Result<()> {
+        // Load the current counter value.
         let counter = &mut ctx.accounts.counter;
         msg!("Previous counter: {}", counter.count);
+
+        // Increment the counter value and log it.
         counter.count = counter.count.checked_add(1).unwrap();
         msg!("Counter incremented. Current count: {}", counter.count);
         Ok(())
@@ -24,7 +33,11 @@ pub mod anchor_counter {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
+    #[account(
+        init, 
+        payer = user, 
+        space = ANCHOR_DISCRIMINATOR + Counter::INIT_SPACE,
+    )]
     pub counter: Account<'info, Counter>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -39,6 +52,7 @@ pub struct Update<'info> {
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Counter {
     pub count: u64,
 }
